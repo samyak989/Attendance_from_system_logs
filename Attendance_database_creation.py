ac5cd74889f3bde8a1afcfe130ab6dc31a6f9ce9
samyak989 <samyak989@gmail.com>
2020-08-31 02:41:08 +0530

    date and time different columns created

--- Attendance_database_creation.py
+++ Attendance_database_creation.py
@@ -5,7 +5,8 @@ import sqlite3
 import os
 
 #location of log files
-data_path = os.path.join(os.path.dirname(__file__),'Data/logs_20200827-1619.csv')
+data_path = os.path.join(os.path.dirname(__file__),'Data')
+#data_path = os.path.join(os.path.dirname(__file__),'Data/logs_20200827-1619.csv')
 #location of datbase file
 database_path = os.path.join(os.path.dirname(__file__),'Database/Attendance_database.db')
 
@@ -15,8 +16,11 @@ c = conn.cursor()
 #creating attendance table
 c.execute('CREATE TABLE ATTENDANCE (Time TEXT, User_full_name TEXT, IP_address TEXT, Roll_no INT, Course_id INT)')
 
+
 #reading log file
-df = pd.read_csv(data_path)
+files_list = os.listdir(data_path)
+df = pd.concat([pd.read_csv(os.path.join(data_path,f)) for f in files_list])
+#df = pd.read_csv(data_path)
 
 #Converting to datetime object
 df['Time'] = df['Time'].apply(lambda x : datetime.strptime(x, '%d/%m/%y, %H:%M'))
@@ -37,11 +41,17 @@ dummy['Roll_no'] = dummy['Roll_no'].astype('int64')
 dummy['Course_id'] = dummy['Description'].str.findall(r'(\d+)').apply(lambda x : x[-1])
 dummy['Course_id'] = dummy['Course_id'].astype(int)
 
+#extracting date
+dummy['Date']=dummy['Time'].dt.date
+
+#extracting time
+dummy['Time']=dummy['Time'].dt.time
+
 #dropping columns
 dummy.drop(['User full name','Affected user','Event context','Component','Origin','Description','Event name'],axis=1,inplace = True)
 
 #rearranging columns
-dummy = dummy[['Name', 'Roll_no', 'Time', 'Course_id', 'IP address']]
+dummy = dummy[['Name', 'Roll_no', 'Date', 'Time', 'Course_id', 'IP address']]
 
 #giving datafrme to database
 dummy.to_sql('ATTENDANCE', conn, if_exists='replace', index = False)
